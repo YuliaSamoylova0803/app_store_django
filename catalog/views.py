97603f1952f96f0e2cbc9880da6ce184043ff810
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-03-20 15:34:10 +0500

    update file views for home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 def home(request):
     return render(request, "catalog/home.html")
@@ -12,3 +13,19 @@ def contacts(request):
 
         return HttpResponse(f"Спасибо, {name}. Сообщение получено!")
     return render(request, "catalog/contacts.html")
+
+
+def home_view(request):
+    # Выборка последних 5 созданных продуктов
+    latest_products = Product.objects.order_by('-created_at')[:5]
+
+    # Вывод в консоль
+    for product in latest_products:
+        print(f"Product: {product.name}, Created at: {product.created_at}")
+
+    # Передача данных в шаблон (если нужно отобразить на странице)
+    context = {
+        'latest_products': latest_products
+    }
+
+    return render(request, 'home.html', context)
