cd6cbe9ec70238a638d79e3a1ed6edb9c426f2ab
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-04-01 17:52:40 +0500

    add blog views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from blog.models import Blog
 
 # Create your views here.
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ["title", "content", "preview", "is_published"]
+    success_url = reverse_lazy("blog:blog_list")
+
+
+class BlogListView(ListView):
+    model = Blog
+
+
+class BlogDetailView(DeleteView):
+    model = Blog
+
+
+class BlogUpdateViews(UpdateView):
+    model = Blog
+    fields = ["title", "content", "preview", "created_at", "is_published"]
+    success_url = reverse_lazy("blog:blog_list")
+
+
+class BlogDeleteViews(DeleteView):
+    model = Blog
+    success_url = reverse_lazy("blog:blog_list")
