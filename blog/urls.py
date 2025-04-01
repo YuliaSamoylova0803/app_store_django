77dcf11d064f54da21f4a5e33d3c575dbc9986a4
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-04-01 17:53:20 +0500

    add blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-
+from blog.views import BlogCreateView, BlogDetailView, BlogListView, BlogUpdateViews, BlogDeleteViews
 
 app_name = BlogConfig.name
 
 urlpatterns = [
+    path("blogs/new/", BlogCreateView.as_view(), name="blog_create"),
+    path("blogs/blog_list/", BlogListView.as_view(), name="blog_list"),
+    path("blogs/blog_detail/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
+    path("blogs/<int:pk>/update/", BlogUpdateViews.as_view(), name="blog_update"),
+    path("blogs/<int:pk>/delete/", BlogDeleteViews.as_view(), name="blog_delete")
 
 ]
