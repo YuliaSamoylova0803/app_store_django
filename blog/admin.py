bc921bd152f521e5ad7369a7c3a2d9eb5e1504fa
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-04-03 09:30:47 +0500

    add blog in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Blog
 
 # Register your models here.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("title", "content", "preview", "is_published")
+    list_filter = ("title",)
+    search_fields = ("title", "content",)
