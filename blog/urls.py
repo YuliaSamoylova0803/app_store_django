d35d94743ac92eb71c4ea619c62e0c0f03199506
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-04-04 10:19:19 +0500

    update urls in blog and config

--- blog/urls.py
+++ blog/urls.py
@@ -6,10 +6,10 @@ from blog.views import BlogCreateView, BlogDetailView, BlogListView, BlogUpdateV
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path("blogs/new/", BlogCreateView.as_view(), name="blog_create"),
-    path("blogs/blog_list/", BlogListView.as_view(), name="blog_list"),
-    path("blogs/blog_detail/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
-    path("blogs/<int:pk>/update/", BlogUpdateViews.as_view(), name="blog_update"),
-    path("blogs/<int:pk>/delete/", BlogDeleteViews.as_view(), name="blog_delete")
+    path("new/", BlogCreateView.as_view(), name="blog_create"),
+    path("blog_list/", BlogListView.as_view(), name="blog_list"),
+    path("blog_detail/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
+    path("<int:pk>/update/", BlogUpdateViews.as_view(), name="blog_update"),
+    path("<int:pk>/delete/", BlogDeleteViews.as_view(), name="blog_delete")
 
 ]
--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
-    path("", include("blog.urls", namespace="blog")),
+    path("blogs/", include("blog.urls", namespace="blog")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
