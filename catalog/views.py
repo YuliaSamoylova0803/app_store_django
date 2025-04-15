332d30cbce61f1f1586f12ff16fee2396345f642
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-04-15 15:49:17 +0500

    update views in catalog

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+
 from .models import Product
 from .forms import ProductForms
 from django.views.generic import ListView, DeleteView, DetailView, TemplateView
@@ -75,7 +77,28 @@ class HomeViewView(ListView):
 class ProductListView(ListView):
     model = Product
 
+
 # app_name/<model_name>_action
 # catalog/product_detail
 class ProductDetailView(DetailView):
     model = Product
+
+
+class ProductCreateViews(CreateView):
+    model = Product
+    form_class = ProductForms
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:product_list")
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForms
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:product_list")
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('catalog:product_list')
