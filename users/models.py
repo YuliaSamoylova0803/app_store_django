7034315b28430a5975ff169538ed904f77fc2d6a
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-04-22 14:32:44 +0500

    update model in app users

--- users/models.py
+++ users/models.py
@@ -5,10 +5,10 @@ from django.core.validators import RegexValidator
 
 # Create your models here
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, verbose_name="Введите адрес электронной почты")
     phone_number = models.CharField(max_length=15, blank=True, null=True, validators=[RegexValidator(r'^\+?1?\d{9,15}$')])
-    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True, help_text="Изображение должно быть квадратным (рекомендуемый размер 200x200)")
-    country = models.CharField(max_length=50, blank=True, null=True)
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True, verbose_name="Изображение пользователя", help_text="Изображение должно быть квадратным (рекомендуемый размер 200x200)")
+    country = models.CharField(max_length=50, blank=True, null=True, verbose_name="Введите страну")
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username",]
