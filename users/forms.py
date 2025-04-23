7e56e466f6bd197689c130f8189c248648187a9e
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-04-23 09:34:55 +0500

    update form for CustomUserCreationForm

--- users/forms.py
+++ users/forms.py
@@ -3,15 +3,20 @@ from django.contrib.auth.forms import UserCreationForm
 
 from .models import CustomUser
 
+
 class CustomUserCreationForm(UserCreationForm):
     phone_number = forms.CharField(max_length=15, required=False,
                                    help_text="Необязательное поле: введите ваш номер телефона.")
     username = forms.CharField(max_length=50, required=True)
-    usable_password = None
+    avatar = forms.ImageField(required=False, help_text="Изображение должно быть квадратным (рекомендуемый размер 200x200)")
+    # usable_password = None
+    password1 = forms.CharField(label="Пароль", widget=forms.PasswordInput, help_text="Ваш пароль должен содержать как минимум 8 символов.")
+    password2 = forms.CharField(label="Подтверждение пароля", widget=forms.PasswordInput, help_text="Введите тот же пароль для подтверждения.")
+
 
     class Meta:
         model = CustomUser
-        fields = ("avatar", "email", "username", "phone_number", "password", "password2")
+        fields = ("avatar", "email", "username", "phone_number")
 
     def clean_phone_number(self):
         phone_number = self.cleaned_data.get("phone_number")
