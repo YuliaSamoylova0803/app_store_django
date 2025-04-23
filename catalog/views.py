4928233322fcb14198954f10fa4a856e7f36c673
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-04-23 10:34:07 +0500

    update views in catalog app

--- catalog/views.py
+++ catalog/views.py
@@ -12,13 +12,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 # app_name/<model_name>_action
 # catalog/home
-class HomeView(FormMixin, ListView):
+class HomeView(LoginRequiredMixin, FormMixin, ListView):
     template_name = 'catalog/home.html'
     form_class = ProductForms
     model = Product
     paginate_by = 6
     ordering = ['id']
     context_object_name = 'products'
+    login_url = '/users/login/'
 
     def get_success_url(self):
         return self.request.path
@@ -52,7 +53,11 @@ def test_pagination(request):
 
 
 class ContactsView(TemplateView):
+    """
+    Страница контактов (общедоступная)
+    """
     template_name = 'catalog/contacts.html'
+
 
     def post(self, request, *args, **kwargs):
         name = request.POST.get('name')
@@ -62,7 +67,7 @@ class ContactsView(TemplateView):
 
 # app_name/<model_name>_action
 # catalog/contacts
-class HomeViewView(ListView):
+class HomeViewView(LoginRequiredMixin, ListView):
     template_name = 'home.html'
     context_object_name = 'latest_products'
     queryset = Product.objects.order_by('created_at')[:5]
@@ -76,23 +81,47 @@ class HomeViewView(ListView):
 # app_name/<model_name>_action
 # catalog/product_list
 class ProductListView(ListView):
+    """
+    Главная страница - список товаров (общедоступная)
+    """
     model = Product
+    template_name = 'catalog/product_list.html'
+    context_object_name = 'products'
+    paginate_by = 10
+    ordering = ['-created_at']  # Сортировка по дате создания (новые сначала)
 
 
 # app_name/<model_name>_action
 # catalog/product_detail
 class ProductDetailView(LoginRequiredMixin, DetailView):
+    """
+    Детали товара (только для авторизованных)
+    """
     model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
+    login_url = '/users/login/'
 
 
 class ProductCreateViews(LoginRequiredMixin, CreateView):
+    """
+    Создание товара (только для авторизованных)
+    """
     model = Product
     form_class = ProductForms
     template_name = "catalog/product_form.html"
     success_url = reverse_lazy("catalog:product_list")
+    login_url = '/users/login/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user  # Привязываем товар к пользователю
+        return super().form_valid(form)
 
 
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    """
+    Редактирование товара (только для авторизованных)
+    """
     model = Product
     form_class = ProductForms
     template_name = "catalog/product_form.html"
@@ -100,6 +129,10 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
 
 
 class ProductDeleteView(LoginRequiredMixin, DeleteView):
+    """
+    Удаление товара (только для авторизованных)
+    """
     model = Product
     template_name = 'catalog/product_confirm_delete.html'
     success_url = reverse_lazy('catalog:product_list')
+    login_url = '/users/login/'
